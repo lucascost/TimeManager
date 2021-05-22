a873072a6c791aac9ac3dfd6bd6585ee6f3fca9f
lucascost <lucas.yan97@outlook.com>
2021-05-22 17:03:44 -0300

    config_dialog adicionado task nomeada

--- app/src/main/java/com/webdicas/timemanager/MainActivity.kt
+++ app/src/main/java/com/webdicas/timemanager/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.webdicas.timemanager
 
+import android.app.Dialog
 import android.app.TimePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.webdicas.timemanager.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 import java.util.*
 
 
-class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
+class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener{
     lateinit var b: ActivityMainBinding
     private var display:TextView? = null
     private var hora_inicio:TextView? = null
@@ -21,23 +24,48 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
          b=  ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
-
-
         display = findViewById(R.id.main_timer)
         hora_inicio=findViewById(R.id.hora_inicio)
         hora_fim=findViewById(R.id.hora_fim)
+
+
 
         this.findViewById<ImageButton>(R.id.edit).setOnClickListener{
             if(control<2)
                 getFullTime()
         }
         b.floatingActionButton.setOnClickListener{
-            val panel = findViewById<LinearLayout>(R.id.panel)
-            val view = layoutInflater.inflate(R.layout.row,panel,false)
-            panel.addView(view)
+            show_dialog()
         }
     }
+    private fun show_dialog(){
+        val dialog= Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.config_task)
+        val name:TextView=dialog.findViewById(R.id.task_name)
+        var duracao=""
 
+        val hora:NumberPicker=dialog.findViewById(R.id.hora)
+        val minuto:NumberPicker=dialog.findViewById(R.id.minuto)
+
+        hora.minValue=0;hora.maxValue=5
+        minuto.minValue=0 ; minuto.maxValue=60
+        dialog.setCancelable(true)
+        dialog.findViewById<Button>(R.id.button).setOnClickListener{
+            add_task(name.text.toString(),4)
+            dialog.dismiss()
+        }
+
+        dialog.show()
+        /*
+        hora.minValue=0 ; hora.maxValue=24
+        minuto.minValue=0 ; minuto.maxValue=60
+         */
+
+
+
+
+    }
     private fun getFullTime(){
         val cal:Calendar = Calendar.getInstance()
         var h=cal.get(Calendar.HOUR)
@@ -53,5 +81,12 @@ class MainActivity : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
             }
         }
         control++
+    }
+    private fun add_task(nome:String,duracao:Int){
+        val panel = findViewById<LinearLayout>(R.id.panel)
+        val view = layoutInflater.inflate(R.layout.row,panel,false)
+        view.findViewById<TextView>(R.id.name).setText(nome)
+        view.findViewById<TextView>(R.id.timer).setText(format(10,0))
+        panel.addView(view)
     }
 }
